d6f755c13c804fd28951e6263f38144d22a91051
Skhsouravhalder <skhsouravhalder@gmail.com>
2025-10-25 19:43:43 +0600

    Create Components Folder

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+  return <div>im footer</div>;
+};
+
+export default Footer;
---
+++ src/Components/Header/Navbar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Navbar = () => {
+  return <div>im nav</div>;
+};
+
+export default Navbar;
